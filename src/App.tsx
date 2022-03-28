32ba743030f2991cb8f1f41d39d55cbb527e91d5
Syakir Rahman <syakirurahman@gmail.com>
2022-03-28 19:29:49 +0700

    Removed console.log

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,6 @@ import UserFollowers from './pages/user/UserFollowers';
 import UserFollowing from './pages/user/UserFollowing';
 
 function App() {
-  console.log(process.env)
   return (
     <div className="App">
       <Router>
